cfb090b16f31c3a417bac886b585225931ab9b80
kesimeg <48391912+kesimeg@users.noreply.github.com>
2020-01-08 20:01:50 +0300

    Update dlib_landmark.py

--- Dataset Creator Codes/dlib_landmark.py
+++ Dataset Creator Codes/dlib_landmark.py
@@ -1,3 +1,6 @@
+"""
+Landmark extractor
+"""
 import cv2
 import numpy as np
 import dlib
@@ -8,7 +11,7 @@ import scipy.ndimage
 from collections import OrderedDict
 import data_augment
 
-def align_face(image,landmarks): #aling face
+def align_face(image,landmarks): 
 
     left_eye_range=range(36, 42)
     right_eye_range=range(42, 48)
@@ -38,8 +41,6 @@ def align_face(image,landmarks): #aling face
     dY = eye1_center[1] - eye2_center[1]
     dX = eye1_center[0] - eye2_center[0]
     angle = np.degrees(np.arctan2(dY, dX)) - 180
-    #print(angle)
-    #if abs(angle)<20:
     rotated=scipy.ndimage.interpolation.rotate(image,angle)
 
     return rotated
@@ -63,7 +64,7 @@ def land_marks(img,landmark_dict):
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     face = detector(gray)
-    #print(face)
+    
     landmark_list=[]
     if len(face)==0:
         for n in range(0,68):
@@ -88,43 +89,3 @@ def land_marks(img,landmark_dict):
             landmark_dict["point_"+str(n)+"y"]=y
 
     return landmark_dict,landmark_list,True
-
-
-
-
-
-
-
-"""
-img = cv2.imread("P030_Anger_img1.jpeg")
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-faces = detector(gray)
-for face in faces:
-    x1 = face.left()
-    y1 = face.top()
-    x2 = face.right()
-    y2 = face.bottom()
-    #cv2.rectangle(img, (x1, y1), (x2, y2),(0, 255, 0),3)
-
-landmarks = predictor(gray, face)
-#'num_parts', 'part', 'parts', 'rect']
-
-print(landmarks.num_parts)
-for n in range(0,landmarks.num_parts):
-    x = landmarks.part(n).x
-    y = landmarks.part(n).y
-    cv2.circle(img, (x, y), 1, (255, 0, 0), -1)
-
-cropped_img=img[y1:y2,x1:x2]
-
-
-_,a,_=land_marks(img,get_landmark_dict())
-print(len(a))
-print(a)
-
-plt.imshow(img)
-plt.show()
-"""
